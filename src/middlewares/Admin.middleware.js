3787991a924d36c183b7795d4343b5214bd1f5fb
nahom zenebe <nahomzenebe123@gmail.com>
2026-01-16 17:25:35 +0300

    adding buyer and seller middleware

--- src/middlewares/Admin.middleware.js
+++ src/middlewares/Admin.middleware.js
@@ -17,3 +17,50 @@ export async function AdminMiddleware(req, res, next) {
     next(err);
   }
 }
+
+
+
+export async function SellerMiddleware(req,res,next){
+  try{
+
+    const user=req.user;
+
+    if (!user) {
+      throw new CustomError("User not authenticated", 401);
+    }
+
+
+    if (user.role !== "Seller") {
+      throw new CustomError("Access denied: Seller only", 403);
+    }
+
+    next()
+
+  }
+  catch(error){
+    next(error)
+  }
+}
+
+
+export async function BuyerMiddleware(req,res,next){
+  try{
+
+    const user=req.user;
+
+    if (!user) {
+      throw new CustomError("User not authenticated", 401);
+    }
+
+
+    if (user.role !== "Buyer") {
+      throw new CustomError("Access denied: Buyer only", 403);
+    }
+
+    next()
+
+  }
+  catch(error){
+    next(error)
+  }
+}
