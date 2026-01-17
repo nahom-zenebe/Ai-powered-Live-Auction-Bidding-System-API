04a23c4ee91f78b39c0910ed74b0c952fc9e1e25
nahom zenebe <nahomzenebe123@gmail.com>
2026-01-17 23:13:58 +0300

    creating a activityrecord utils

---
+++ src/utils/Activity.js
@@ -0,0 +1,21 @@
+import Activity from '../models/Activity.model.js'
+
+
+export async function RecordActivity(activitys){
+
+    try{
+
+        if(!activitys){
+            throw new Error();
+        }
+
+        const newActivitys=await Activity(activitys)
+
+        await newActivitys.save()
+
+    }
+    catch(err){
+        console.log(err)
+    }
+
+}
